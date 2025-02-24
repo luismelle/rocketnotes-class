be45b54554438449bdd9484497bb393ddcfe8af1
luismelle <felipemelle@gmail.com>
2025-02-23 22:02:01 -0300

    Aula de JSX excluindo alguns arquivos e criando hello world

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { Details } from './pages/Details.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Details />
   </StrictMode>,
 )
