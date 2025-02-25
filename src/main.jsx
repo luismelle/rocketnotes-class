0c3007b67b1fc0cbfca7ac9d541003e1c1cfe0aa
luismelle <felipemelle@gmail.com>
2025-02-25 00:24:28 -0300

    Criando arquivo de estilo e estilo global

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ThemeProvider } from 'styled-components'
+import GlobalStyles from './styles/global'
 import { Details } from './pages/Details/'
+
+import theme from './styles/theme'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Details />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Details />
+    </ThemeProvider>
   </StrictMode>,
 )
