a00e4ab2b2d9a3f2206738527a6527b5043ad8ad
luismelle <felipemelle@gmail.com>
2025-02-25 19:16:28 -0300

    Component and property create (buttons with props)

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,11 +1,17 @@
 import { Container } from "./style.js"
 
+import { Button } from "../../components/Button/index.jsx"
+
 export function Details() {
 
   return (
     <Container>
       <h1>Hello world!</h1>
       <span>TESTEEEEEEEEE</span>
+
+      <Button title="Entrar" />
+      <Button title="Cadastrar" />
+      <Button title="Sair" />
     </Container>
     
   )
