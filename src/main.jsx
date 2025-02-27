05a079c1c9700d10fef5a01312588d8dc6221486
luismelle <felipemelle@gmail.com>
2025-02-27 00:29:43 -0300

    home creation, Grid home application

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyles from './styles/global'
-import { Details } from './pages/Details/'
 
 import theme from './styles/theme'
+
+import { Home } from './pages/Home/'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Details />
+      <Home />
     </ThemeProvider>
   </StrictMode>,
 )
